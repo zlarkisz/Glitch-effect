3c6f4370e28c289995a2c18e63ebc44244730042
bufin <bufin@semlt.com>
2019-01-30 18:46:58 +0200

    glitch

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="glitch">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style lang="less">
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  .glitch {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    background: url(assets/211278.jpg) center/cover no-repeat;
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: url(assets/211278.jpg) center/cover no-repeat;
+      // opacity: .5;
+      mix-blend-mode: hard-light;
+      animation: animate .2s infinite; 
+    }
+    &:hover:before {
+      
+    }
+
+    @keyframes animate {
+      0% {
+        background-position: 0 0;
+        filter: hue-rotate(0deg);
+      }
+      10% {
+        background-position: 4px 0;
+      }
+      20% {
+        background-position: -4px 0;
+      }
+      30% {
+        background-position: 12px 0;
+      }
+      40% {
+        background-position: -16px 0;
+      }
+      50% {
+        background-position: -24px 0;
+      }
+      60% {
+        background-position: -52px 0;
+      }
+      70% {
+        background-position: 0 22px;
+      }
+      80% {
+        background-position: -40px -10px;
+      }
+      100% {
+        background-position: 0 0;
+        filter: hue-rotate(360deg);
+
+      }
+    }
+  }
 }
 </style>
